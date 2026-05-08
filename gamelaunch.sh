2fec7218548cdd6d382d2b0dd9780c6d76bb7feb
Louis <louis.chevalier@outlook.com>
2026-05-08 18:08:49 -0400

    read from gameinfo file and start up game from both path and steam id

--- gamelaunch.sh
+++ gamelaunch.sh
@@ -1,1 +1,27 @@
-/home/Louis/.local/share/Steam/steamapps/common/Factorio/bin/x64/factorio
+#!/bin/bash
+input="./gameinfo.txt"
+line=""
+i=0
+declare -a PROPERTIES
+while IF=: read -r prop val
+do
+	((i++))
+	echo $prop
+	echo $val
+	PROPERTIES[i]="$val"
+	#line << $in
+done < "$input"
+
+echo $i
+
+if [ -x "${PROPERTIES[1]}" ]; then
+        "${PROPERTIES[1]}"
+else
+	echo "Game is not executable"
+fi
+
+"${PROPERTIES[1]}"
+#steam "${PROPERTIES[2]}"
+echo "Done"
+#steam steam://run/427520
+#/home/Louis/.local/share/Steam/steamapps/common/Factorio/bin/x64/factorio
